cfaac92ad4fe244ea2c5a7689029904a9aac4b72
Guilty <981196023@qq.com>
2025-05-10 12:58:27 +0800

    灰度检测上台

--- test.py
+++ test.py
@@ -2,6 +2,22 @@ import uptech
 import time
 flag=0
 cnt=0
+def straight():
+    up.CDS_SetSpeed(1, 500)
+    up.CDS_SetSpeed(2, 500)
+
+def back():
+    up.CDS_SetSpeed(1, -700)
+    up.CDS_SetSpeed(2, -1000)
+
+def left():
+    up.CDS_SetSpeed(1, -1000)
+    up.CDS_SetSpeed(2, 1000)
+
+def right():
+    up.CDS_SetSpeed(1, 1000)
+    up.CDS_SetSpeed(2, -1000)
+
 if __name__ == "__main__":
     up = uptech.UpTech()
     up.LCD_Open(2)
@@ -10,7 +26,7 @@ if __name__ == "__main__":
     up.ADC_Led_SetColor(0, 0xE67E223)#FF0000(纯红)FF5733(橙红)C70039(深红)000000(纯黑)FFFF00(纯黄)
     up.ADC_Led_SetColor(1, 0x800080)#0000FF(纯蓝)3498DB(天蓝)2C3E50(深蓝)E67E22(萝卜橙)8B4513(马棕)
                                                #F9E79F(纯白)00FF00(纯绿色)800080(纯紫)E67E223(青色)
-
+    io_data = []
     up.CDS_SetMode(1, 1)
     up.CDS_SetMode(2, 1)
     up.CDS_SetMode(3, 0)#最低 700 最高 400 平放 550
@@ -21,32 +37,55 @@ if __name__ == "__main__":
     # FONT_12X20  = 11
     print("test succeed")
     while True:
+        io_all_input = up.ADC_IO_GetAllInputLevel()
+        io_array = '{:08b}'.format(io_all_input)
+        io_data.clear()
+
+        for index, value in enumerate(io_array):
+            io = (int)(value)
+            io_data.insert(0, io)
+        IO_0=io_data[0]
+        IO_1=io_data[1]
+        IO_3=io_data[3]
+        IO_4=io_data[4]
+        IO_6=io_data[5]
+        IO_7=io_data[6]
         adc_value = up.ADC_Get_All_Channle()
-        result = "[{}]".format(",".join(map(str, adc_value)))
+        # result = "[{}]".format(",".join(map(str, adc_value)))
 
         up.LCD_SetFont(up.FONT_12X20)
         up.LCD_SetForeColor(up.COLOR_GBLUE)
         up.LCD_PutString(0, 0, 'Go North All')
+
         up.LCD_SetFont(up.FONT_12X20)
         up.LCD_SetForeColor(up.COLOR_YELLOW)
-        up.LCD_PutString(0, 20, f'{result}')
-        up.LCD_Refresh()
+        up.LCD_PutString(0, 20, f'{adc_value}')
 
-        up.CDS_SetSpeed(1,650)
-        up.CDS_SetSpeed(2,700)
-        if flag == 0:
+        up.LCD_Refresh()    #185,222
+                            #315,306
+        back()
+        if adc_value[0]<270 and adc_value[1]<270:
             up.CDS_SetAngle(3,600,500)
             up.CDS_SetAngle(4,300,500)
         else:
             up.CDS_SetAngle(3,700,500)#最低
             up.CDS_SetAngle(4,200,500)#最低
-        # up.CDS_SetAngle(3,700,500)#最低
-        # up.CDS_SetAngle(4,200,500)#最低
-        # up.CDS_SetAngle(3,400,500)#最高
-        # up.CDS_SetAngle(4,500,500)#最高
 
+        # if IO_3==0 and IO_4==0:
+        #     if IO_0 ==0 and IO_1 ==1:
+        #         left()
+        #     elif IO_0==1 and IO_1==0:
+        #         right()
+        #     else:
+        #         straight()
+        # elif IO_3==1 and IO_4==0:
+        #     right()
+        # elif IO_3 == 0 and IO_4 == 1:
+        #     left()
+        # else:
+        #     back()
         cnt+=1
-        print(f'adc{result}')
+        print(f'adc{adc_value}')
 
 
 
